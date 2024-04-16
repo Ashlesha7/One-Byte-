6349a82fa67aa6a6994a512b8ab134d119ea88ea
Maharjan Binisha <np03cs4a220375@heraldcollege.edu.np>
2024-04-16 09:45:14 +0545

    binisha commit

--- api/index.php
+++ api/index.php
@@ -1,145 +1,117 @@
-<!--DOCTYPE html>
+<?php 
+    // Start session to use session variables
+    session_start();
 
-<html lang="en" >
-<head>
-  <meta charset="UTF-8">
-  <title>PHP Learning</title>
-  
-  
-  <link rel='stylesheet prefetch' href='https://fonts.googleapis.com/css?family=Open+Sans:600'>
-      <link rel="stylesheet" href="./assets/css/style.css">
-</head>
-<body>
-  <div class="login-wrap">
-  <div class="login-html">
-    <input id="tab-1" type="radio" name="tab" class="sign-in" checked><label for="tab-1" class="tab">Sign In</label>
-    <input id="tab-2" type="radio" name="tab" class="sign-up"><label for="tab-2" class="tab">Sign Up</label>
-    <div class="login-form">
-      <form class="sign-in-htm" action="../api/user/login.php" method="POST">
-        <div class="group">
-          <label for="user" class="label">Username</label>
-          <input id="username" name="username" type="text" class="input">
-        </div>
-        <div class="group">
-          <label for="pass" class="label">Password</label>
-          <input id="password" name="password" type="password" class="input" data-type="password">
-        </div>
-        <div class="group">
-          <input id="check" type="checkbox" class="check" checked>
-          <label for="check"><span class="icon"></span> Keep me Signed in</label>
-        </div>
-        <div class="group">
-          <input type="submit" class="button" value="Sign In">
-        </div>
-        <div class="hr"></div>
-        <div class="foot-lnk">
-          <a href="#forgot">Forgot Password?</a>
-        </div>
-      </form>
-      <form class="sign-up-htm" action="../api/user/signup.php" method="POST">
-        <div class="group">
-          <label for="user" class="label">Username</label>
-          <input id="username" name="username" type="text" class="input">
-        </div>
-        <div class="group">
-          <label for="pass" class="label">Password</label>
-          <input id="password" name="password" type="password" class="input" data-type="password">
-        </div>
-        <div class="group">
-          <label for="pass" class="label">Confirm Password</label>
-          <input id="pass" type="password" class="input" data-type="password">
-        </div>
-        <div class="group">
-          <input type="submit" class="button" value="Sign Up">
-        </div>
-        <div class="hr"></div>
-        <div class="foot-lnk">
-          <label for="tab-1">Already Member?</a>
-        </div>
-      </form>
-    </div>
-  </div>
-</div>
-  
-  
-</body>
-</html>
+    // Include database connection and functions files
+    require "includes/db.php";
+    require "includes/functions.php";
 
--->
+    // Redirect to food_list.php if user is already logged in
+    if(isset($_SESSION['user'])) {
+        header("location: food_list.php");
+    }
 
-<!DOCTYPE html>
+    // Check if the form is submitted via POST
+    if($_SERVER['REQUEST_METHOD'] == 'POST') {
+        
+        // Check if the submit button is pressed
+        if(isset($_POST['submit'])) {
+            
+            // Sanitize and store the username to prevent SQL injection
+            $user = escape($_POST['username']);
+            // Store password directly (note: consider using password hashing for better security)
+            $pass = $_POST['password'];
+
+            // Check if username and password are not empty
+            if($user != "" && $pass != "") {
+                
+                // SQL query to verify login credentials from the 'admin' table
+                $verify = $db->query("SELECT * FROM admin WHERE username='$user' AND password='$pass' LIMIT 1");
+                
+                // Check if the credentials are correct and fetch the user details
+                if($verify->num_rows) {
+                    
+                    $row = $verify->fetch_assoc();
+                    
+                    // Set the user session and redirect to food_list.php
+                    $_SESSION['user'] = $row['username'];
+                    header("location: food_list.php");
+                    
+                } else {
+                    
+                    // Alert message for invalid login credentials
+                    echo "<script>alert('Invalid login credentials. Please try again')</script>";
+                    
+                }
+                
+            } else {
+                
+                // Alert message for empty fields
+                echo "<script>alert('Some fields are empty. All fields required!')</script>";
+                
+            }
+            
+        }
+        
+    }
+    
+?>
+
+<!doctype html>
 <html lang="en">
 <head>
-  <meta charset="UTF-8">
-  <title>PHP Learning</title>
-  <link rel='stylesheet prefetch' href='https://fonts.googleapis.com/css?family=Open+Sans:600'>
-  <link rel="stylesheet" href="./assets/css/style.css">
+    <meta charset="utf-8" />
+    <link rel="icon" type="image/png" href="assets/img/favicon.ico">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+
+    <title>One Byte</title>
+
+    <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
+    <meta name="viewport" content="width=device-width" />
+
+    <!-- Bootstrap core CSS     -->
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet" />
+
+    <!-- Animation library for notifications   -->
+    <link href="assets/css/animate.min.css" rel="stylesheet"/>
+
+    <!--  Light Bootstrap Table core CSS    -->
+    <link href="assets/css/light-bootstrap-dashboard.css" rel="stylesheet"/>
+
+    <!--     Fonts and icons     -->
+    <link href="assets/css/pe-icon-7-stroke.css" rel="stylesheet" />
+    <link href="assets/css/style.css" rel="stylesheet" />
 </head>
 <body>
-  <div class="login-wrap">
-    <div class="login-html">
-      <input id="tab-1" type="radio" name="tab" class="sign-in" checked><label for="tab-1" class="tab">Sign In</label>
-      <input id="tab-2" type="radio" name="tab" class="sign-up"><label for="tab-2" class="tab">Sign Up</label>
-      
-      <div class="login-form">
-        <form class="sign-in-htm" action="../api/user/login.php" method="POST">
-          <div class="group">
-            <label for="user" class="label">Username</label>
-            <input id="username" name="username" type="text" class="input">
-          </div>
-          <div class="group">
-            <label for="pass" class="label">Password</label>
-            <input id="password" name="password" type="password" class="input" data-type="password">
-          </div>
-          <div class="group">
-            <input id="check" type="checkbox" class="check" checked>
-            <label for="check"><span class="icon"></span> Keep me Signed in</label>
-          </div>
-          <div class="group">
-            <input type="submit" class="button" value="Sign In">
-          </div>
-          <div class="hr"></div>
-          <div class="foot-lnk">
-            <a href="#forgot">Forgot Password?</a>
-          </div>
+
+<div class="login_wrapper">
+    
+    <div class="login_holder">
+            
+        <form method="post" action="index.php">
+            
+            <div class="header">
+                <h4 style="border-bottom: 1px solid #FF5722;" class="title">Login Form</h4>
+            </div>
+            
+            <div class="form-group">
+                <label>Username</label>
+                <input type="text" name="username" class="form-control" placeholder="Enter Username" autofocus>
+            </div>
+            
+            <div class="form-group">
+                <label>Password</label>
+                <input type="password" name="password" class="form-control" placeholder="Enter your password">
+            </div>
+            
+            <input type="submit" name="submit" value="Login" class="btn btn-info btn-fill pull-right" style="background: #FF5722; border-color: #FF5722;" />
+            <div class="clearfix"></div>
+            
         </form>
-        <form class="sign-up-htm" action="../api/user/signup.php" method="POST">
-          <div class="group">
-            <label for="user" class="label">Username</label>
-            <input id="username" name="username" type="text" class="input">
-          </div>
-          <div class="group">
-            <label for="pass" class="label">Password</label>
-            <input id="password" name="password" type="password" class="input" data-type="password">
-          </div>
-          <div class="group">
-            <label for="pass" class="label">Confirm Password</label>
-            <input id="pass" type="password" class="input" data-type="password">
-          </div>
-          <div class="group">
-            <input type="submit" class="button" value="Sign Up">
-          </div>
-          <div class="hr"></div>
-          <div class="foot-lnk">
-            <label for="tab-1">Already Member?</a>
-          </div>
-        </form>
-        <!-- Add the Admin text with JavaScript for redirection -->
         
-      </div>
     </div>
-   
-  </div>
-  <div class="foot-lnk" id="admin-link">
-  <a href="../../admin/index.php">If you are admin click ->Admin</a>
+    
 </div>
 
-
-  <script>
-    // JavaScript for redirection to admin page
-    document.getElementById('admin-link').addEventListener('click', function() {
-      window.location.href = "../../admin/index.php"; // Redirect to admin login page
-    });
-  </script>
 </body>
 </html>
